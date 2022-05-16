5cf12e0be88afb11feea99a629bbabc749683ecd
Maratkarimov21 <marat.karimov@flatstack.com>
2022-05-16 20:10:35 +0300

    chat update broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat
+
+  after_create_commit -> { broadcast_update_to "chat_#{self.chat.id}", partial: "chats/chat_item", locals: { chat: self.chat }, target: 'chat' }
 end
