933ea59edcebd2bebd895511d3631933a4b2d7e9
Maratkarimov21 <marat.karimov@flatstack.com>
2022-05-19 00:08:30 +0300

    scroll fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat
 
-  after_create_commit -> { broadcast_update_to "chat_#{self.chat.id}", partial: "chats/chat_item", locals: { chat: self.chat }, target: 'chat' }
+  after_create_commit -> {
+    broadcast_append_to "chat_#{self.chat.id}",
+      partial: "chats/message",
+      locals: { message: self, user: self.user },
+      target: 'messages'
+  }
 end
